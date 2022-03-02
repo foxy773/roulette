ca07b0c0af3d814f376f1151f9b33cf1a48887e8
foxy773 <felix_1999@live.no>
2022-03-02 15:03:52 +0100

    :tada: New project

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+	<h1>Roulette</h1>
+	<h2>Results:</h2>
+	<button @click="fetchRoll()"></button>
+
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			
+		}
+	},
+
+	async created() {
+		
+	},
+
+	methods: {
+		async fetchRoll() {
+			
+		}
+	},
+
+	mounted() {
+		
+	}
+}
+</script>
+
